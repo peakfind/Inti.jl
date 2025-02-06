1aa49a6b1de092a1fdb54e927505e2d8d88dd467
jyzhang <peakfind@126.com>
2025-02-06 15:18:32 +0100

    plot the real part of quasi-periodic Green function

---
+++ examples/qpgreen.jl
@@ -0,0 +1,30 @@
+#=
+# fig2_11.jl
+# 
+# Plot the real part of the quasi-periodic Green functions
+# by Ewald's method
+# 
+# Reference: Mathematical and Computational Methods in Photonics 
+#            and Phononics, Fig 2.11
+=#
+
+using CairoMakie
+include("ewald.jl")
+
+function field_real(x, y)
+    k = 2 + 0.5im # wave number
+    α = π/8       # quasi-momentum
+    a = sqrt(π)   # parameter introduced by Ewald's method
+    d = 1         # period
+    N = 5
+    M = 5
+    J = 10
+    return real(ewald(x, y, k, α, d, a, N, M, J))
+end
+
+xs = range(-5, 5, length=200)
+ys = range(-5, 5, length=200)
+zs = [field_real(x, y) for x in xs, y in ys]
+
+fig, ax, hm = heatmap(xs, ys, zs, colormap=:winter)
+fig
